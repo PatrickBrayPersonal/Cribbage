b6a169965d1bf116ac3f8882af04f17d7f486ec3
PatrickBrayPersonal <65143114+PatrickBrayPersonal@users.noreply.github.com>
2020-06-10 08:13:36 -0400

    First Upload

---
+++ CribbageDriver.py
@@ -0,0 +1,7 @@
+from Scoring import Scoring
+score = Scoring()
+class CribbageDriver:
+    def main(self):
+        hand1 = [(1,'S'),(1,'C'),(2,'D'),(2,'H')]
+        cut1 = [(3,'S')]
+        score()
---
+++ Scoring.py
@@ -0,0 +1,5 @@
+class Scoring:
+    def __init__(self):
+        pass
+
+    def fiveCardScore(self, hand, cut):
